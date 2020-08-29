e855aa8c542ee89dbed425499d40793d17445ab6
TheWoops <thewoops.deeplearning@gmail.com>
2020-08-29 19:56:36 +0200

    initial commit 2

---
+++ cv.js
@@ -0,0 +1,64 @@
+$('#navigation a').on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000);
+  });
+  
+  
+  $('.toggler, .nav-content a:not(#dropdown-link)').on('click', function(){
+    $('.toggler').toggleClass('change');
+    $('.nav-content').slideToggle();
+    $('#dropdown-menu').slideUp();
+    $('.menu-overlay').toggle();
+  });
+  
+  $('.nav-content .dropdown').on('click', function(){
+    $('#dropdown-menu').slideToggle();
+  });
+  
+  $('.menu-overlay').on('click', function(){
+    $('.toggler').removeClass('change');
+    $('.nav-content').slideUp();
+    $('#dropdown-menu').slideUp();
+    $('.menu-overlay').hide();
+  });
+  
+  $("#contact input, #contact textarea").on('focusout', function(){
+  
+    var text_val = $(this).val();
+    if (text_val === "") {
+      $(this).removeClass('has-value');
+    } else {
+      $(this).addClass('has-value');
+    }
+  
+  });
+
+  // full page section animations
+  $('.S1').click(() => {
+    $('html, body').animate({
+          scrollTop: $('.S2').offset().top
+    }, 1000);
+});
+$('.S2').click(() => {
+    $('html, body').animate({
+          scrollTop: $('.S3').offset().top
+    }, 1000);
+});
+$('.S3').click(() => {
+    $('html, body').animate({
+          scrollTop: $('.S4').offset().top
+    }, 1000);
+});
+$('.S4').click(() => {
+    $('html, body').animate({
+          scrollTop: $('.S5').offset().top
+    }, 1000);
+});
+$('.S5').click(() => {
+    $('html, body').animate({
+          scrollTop: $('.S1').offset().top
+    }, 1000);
+});
